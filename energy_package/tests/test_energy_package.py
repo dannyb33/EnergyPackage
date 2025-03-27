e44a11002b608dd3e9aee41d4c27742340989872
Daniel Benish <dannyb3@vt.edu>
2025-03-27 11:46:11 -0400

    added test functions

--- energy_package/tests/test_energy_package.py
+++ energy_package/tests/test_energy_package.py
@@ -7,9 +7,59 @@ import sys
 
 import pytest
 
-import energy_package
+import energy_package as ep
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 
 def test_energy_package_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "energy_package" in sys.modules
+    
+def test_energy():
+    """Test energy() Function"""
+    
+    #Create a graph definining ising interactions
+    N = 6
+    Jval = 2.0
+    G = nx.Graph()
+    G.add_nodes_from([i for i in range(N)])
+    G.add_edges_from([(i,(i+1)% G.number_of_nodes() ) for i in range(N)])
+    for e in G.edges:
+        G.edges[e]['weight'] = Jval
+    
+    conf = ep.BitString(N)
+    ham = ep.IsingHamiltonian(G)
+    ham.set_mu(np.array([.1 for i in range(N)]))
+
+    conf.flip_site(2)
+    conf.flip_site(3)
+    e = ham.energy(conf)
+    assert(np.isclose(e, 3.8))
+
+def test_average():
+    """Test compute_average_values() function"""
+    
+    #Create a graph definining ising interactions
+    N = 6
+    Jval = 2.0
+    G = nx.Graph()
+    G.add_nodes_from([i for i in range(N)])
+    G.add_edges_from([(i,(i+1)% G.number_of_nodes() ) for i in range(N)])
+    for e in G.edges:
+        G.edges[e]['weight'] = Jval
+    
+    # Define a new configuration instance for a 6-site lattice
+    conf = ep.BitString(N)
+    ham = ep.IsingHamiltonian(G)
+
+    # Compute the average values for Temperature = 1
+    E, M, HC, MS = ham.compute_average_values(1)
+
+    assert(np.isclose(E,  -11.95991923))
+    assert(np.isclose(M,   -0.00000000))
+    assert(np.isclose(HC,   0.31925472))
+    assert(np.isclose(MS,   0.01202961))
+
